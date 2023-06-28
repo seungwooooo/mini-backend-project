775ce7f06e8a625a950a577f5efc0d4b84e9f3aa
양승우 <ysw3114@douzone.com>
2023-06-28 23:06:20 +0900

    웹소켓config생성, websocket디펜던시 추가

---
+++ src/main/java/com/talk/vo/ChatRoom.java
@@ -0,0 +1,49 @@
+package com.talk.vo;
+
+public class ChatRoom {
+
+	private Long no;
+	private String title;
+	private String contents;
+	private int joinCount;
+	private String createTime;
+	private String type;
+	
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public int getJoinCount() {
+		return joinCount;
+	}
+	public void setJoinCount(int joinCount) {
+		this.joinCount = joinCount;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
